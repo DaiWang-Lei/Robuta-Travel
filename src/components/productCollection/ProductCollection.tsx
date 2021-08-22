7a31144cedcc1e29615388ff83231252427acba0
罗卜大 <7628035+wx_8387aaaa57@user.noreply.gitee.com>
2021-08-22 16:05:09 +0800

    feat:统一商品列表页面书写方式

--- src/components/productCollection/ProductCollection.tsx
+++ src/components/productCollection/ProductCollection.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react'
 import { Col, Row, Divider, Image } from 'antd'
 import ProductImage from './ProductImage'
 import styles from "./ProductCollection.module.css";
-import { ProductImageProps } from './ProductImage';
 
 export interface ProductCollectionProps {
   title: JSX.Element | string;
@@ -26,34 +25,39 @@ export const ProductCollection: FC<ProductCollectionProps> = ({ title, sideImage
         <Col span={20}>
 
           <Row>
-            <Col span={12}>
-              <ProductImage
-                id={products[0].id}
-                title={products[0].title}
-                size='large'
-                imageSrc={products[0].touristRoutePictures[0].url}
-                price={products[0].price}
-              />
-            </Col>
+            {
+              products.slice(0, 1).map((item, index) => {
+                return (
+                  <Col span={12} key={item.id}>
+                    <ProductImage
+                      id={item.id}
+                      title={item.title}
+                      size='large'
+                      imageSrc={item.touristRoutePictures[0].url}
+                      price={item.price}
+                    />
+                  </Col>
+                )
+              })
+            }
             <Col span={12}>
               {
                 <Row>
-
                   {
-                    products.slice(1,5).map((item, index) => {
+                    products.slice(1, 5).map((item, index) => {
                       return (
                         <Col span={12} key={item.id}>
                           <ProductImage
                             id={item.id}
                             title={item.title}
-                            size={item.size}
+                            size='small'
                             imageSrc={item.touristRoutePictures[0].url}
-
                             price={item.price}
                           />
                         </Col>
                       )
-                    })}
+                    })
+                  }
                 </Row>
 
               }
@@ -61,7 +65,7 @@ export const ProductCollection: FC<ProductCollectionProps> = ({ title, sideImage
           </Row>
           <Row>
             {
-               products.slice(5,9).map((item, index) => {
+              products.slice(5, 9).map((item, index) => {
                 return (
                   <Col span={6} key={item.id}>
                     <ProductImage
